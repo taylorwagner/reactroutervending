ba240755ad4f71b4604f13a09c5b474b679cfad6
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-08 21:22:53 -0600

    components in app

--- src/App.js
+++ src/App.js
@@ -11,6 +11,18 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Route exact path="/">
+        <VendingMachine />
+      </Route>
+      <Route exact path="/chocolate">
+        <MilkyWay />
+      </Route>
+      <Route exact path="/sweets">
+        <Skittles />
+      </Route>
+      <Route exact path="/soda">
+        <DrPepper />
+      </Route>
       </BrowserRouter>
     </div>
   );
