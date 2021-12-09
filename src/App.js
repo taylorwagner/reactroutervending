35d3ac21b8fcf29dcd6a6f2a5bf5078dc429cf4c
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-08 21:10:16 -0600

    import components to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import DrPepper from './DrPepper';
+import Skittles from './Skittles';
+import MilkyWay from './MilkyWay';
+import VendingMachine from './VendingMachine';
+
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
