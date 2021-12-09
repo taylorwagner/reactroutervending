823985582eb93ba1626478e1eb1369b6b9b2217e
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-08 21:35:13 -0600

    links to treats

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function VendingMachine() {
     return (
@@ -8,6 +9,9 @@ function VendingMachine() {
                 src="https://images.unsplash.com/photo-1575224526797-5730d09d781d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
                 alt="Choose your treat!"
             />
+            <h2><Link to="/soda">Dr. Pepper</Link></h2>
+            <h2><Link to="/chocolate">Milky Way</Link></h2>
+            <h2><Link to="/sweets">Skittles</Link></h2>
         </div>
     );
 }
